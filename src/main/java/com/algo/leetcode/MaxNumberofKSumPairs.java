f3401370dc45c4bbb584f62dcd7361a648a0c3dc
Anand <anand.k.nigam@gmail.com>
2022-12-08 17:58:59 +0530

    Ksum

--- src/main/java/com/algo/leetcode/MaxNumberofKSumPairs.java
+++ src/main/java/com/algo/leetcode/MaxNumberofKSumPairs.java
@@ -14,23 +14,23 @@ public class MaxNumberofKSumPairs {
 //        findPairWithSum1(new int[]{2,2,2,3,1,1,4,1}, 4);
 //        findPairWithSum1(new int[]{3,1,3,4,3}, 6);
 //        findPairWithSum1(new int[]{1, 2, 3,4}, 5);
-        findPairWithSum1(new int[]{4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4}, 2);
+//        findPairWithSum1(new int[]{4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4}, 2);
+        findPairWithSum1(new int[]{5000000,5000000,5000000,5000000,5000000}, 10000000);
 
 
     }
 
 
     public static int findPairWithSum1(int[] a, int sum){
-        Map<Integer, Stack<Integer>> valuePositionMap = new HashMap<>();
+
+        Map<Integer, Integer> valuePositionMap = new HashMap<>();
 
         for (int i = 0; i < a.length; i++) {
             if(valuePositionMap.containsKey(a[i])) {
-                valuePositionMap.get(a[i]).push(i);
+                valuePositionMap.put(a[i],valuePositionMap.get(a[i])+1);
             }
             else {
-                Stack<Integer> s = new Stack<>();
-                s.push(i);
-                valuePositionMap.put(a[i], s);
+                valuePositionMap.put(a[i], 1);
             }
 
         }
@@ -38,33 +38,45 @@ public class MaxNumberofKSumPairs {
         int ops = 0;
 
 
-        for (Map.Entry<Integer, Stack<Integer>> entry: valuePositionMap.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry: valuePositionMap.entrySet()) {
 
-
-            Stack<Integer> s1 = entry.getValue();
-            if(s1.isEmpty()) {
+            int v1 = entry.getKey();
+            int s1 = entry.getValue();
+            if(s1 == 0) {
                 continue;
             }
-            Integer v1 = entry.getKey();
 
-            Integer v2 = sum - v1;
+            int v2 = sum - v1;
             if(v2 < 0) {
                 continue;
             }
-            // stacks may be same. so it is required to remove pop and then check for emptiness again
-            while (!s1.isEmpty()) {
-                Integer p1 = s1.pop();
 
-                Stack<Integer> s2 = valuePositionMap.get(v2);
-                if (s2 == null || s2.isEmpty()) {
-                    continue;
+            while(s1 > 0) {
+
+                if (v1 == v2) {
+                    if(s1 < 2) {
+                        break;
+                    }
+                    s1 = s1-2;
+                    valuePositionMap.put(v1, s1 );
+                    ops++;
+
                 }
-                Integer p2 = s2.pop();
-                ops++;
-                System.out.println(a[p1] + "," + a[p2] + "   - [" + p1 + "," + p2 + "]");
+                else {
+                    if(!valuePositionMap.containsKey(v2)) {
+                        break;
+                    }
+                    int s2 = valuePositionMap.get(v2);
+                    if (s1 > 0 && s2 > 0) {
+                        ops++; s1= s1-1; s2= s2-1;
+                        valuePositionMap.put(v1, s1 );
+                        valuePositionMap.put(v2, s2 );
+                    }
+                    else {
+                        break;
+                    }
+                }
             }
-
-
 
         }
 
